1b93d45238bdfe0fe6520663c613c32514d4ace2
Alvaro Miranda Aguilera <kikitux+github@gmail.com>
2022-09-09 09:24:55 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,16 +3,19 @@ variable "security_group_name" {
   type        = string
   default     = "terraform-nomad-1server-1client"
 }
+
 variable "ami" {
   description = "The AMI to run in the cluster"
   type        = string
   default = "ami-0c55b159cbfafe1f0"
 }
+
 variable "instance_type" {
   description = "The type of EC2 Instances to run (e.g. t2.micro)"
   type        = string
   default = "t2.micro"
 }
+
 variable "server_port" {
   description = "The port the server will use for HTTP requests"
   type        = number
