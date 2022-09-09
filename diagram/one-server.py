1b0a206c0d15004e8ce4251cbb014ac666414e51
Alvaro Miranda Aguilera <kikitux+github@gmail.com>
2022-09-09 13:55:34 +0200

    Update one-server.py

--- diagram/one-server.py
+++ diagram/one-server.py
@@ -5,11 +5,13 @@ from diagrams import Diagram
 from diagrams import Cluster, Diagram, Edge
 from diagrams.custom import Custom
 from diagrams.aws.compute import EC2
+from diagrams.onprem.compute import Nomad
 
 with Diagram("Simple Nomad cluster", show=False):
 
-    with Cluster("Data Center"):
-        n_server = Custom("Nomad server", "./images.png")
-        n_client = Custom("Nomad Client", "./images.png")
+    with Cluster("VPC"):
+        with Cluster("SubNet"):
+            n_server = Nomad("Nomad server")
+            n_client = Nomad("Nomad Client")
 
-        n_server >> Edge(color="darkgreen" ,label="RPC TCP/4647") << n_client
+            n_server >> Edge(color="darkgreen" ,label="RPC TCP/4647") << n_client
