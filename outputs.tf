1b63ca082d02fae3aa55a412f94a348beecf672f
Alvaro Miranda Aguilera <kikitux+github@gmail.com>
2022-09-09 09:25:24 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -3,10 +3,12 @@ output "private_key_pem" {
   description = "The private key (save this in a .pem file) for ssh to instances"
   sensitive = true
 }
+
 output "server_public_ip" {
   value       = aws_instance.server.public_ip
   description = "The public IP of the EC2 Instance "
 }
+
 output "server_private_ip" {
   value       = aws_instance.server.private_ip
   description = "The private IP of the EC2 Instance "
@@ -16,6 +18,7 @@ output "client_public_ip" {
   value       = aws_instance.client.public_ip
   description = "The public IP of the EC2 Instance client "
 }
+
 output "client_private_ip" {
   value       = aws_instance.client.private_ip
   description = "The private IP of the EC2 Instance client"
